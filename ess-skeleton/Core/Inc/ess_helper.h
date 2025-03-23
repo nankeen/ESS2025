2cb2ddc52b625028b0ef8f3962698a25acf60a48
Xinyu <46095273+XinyuHou97@users.noreply.github.com>
2025-03-23 23:30:42 +0000

    first commit

---
+++ ess-skeleton/Core/Inc/ess_helper.h
@@ -0,0 +1,7 @@
+
+#ifndef INC_ESS_HELPER_H_
+#define INC_ESS_HELPER_H_
+
+void ess_helper_init(void);
+
+#endif /* INC_ESS_HELPER_H_ */
---
+++ ess-skeleton/Core/Src/main.c
@@ -0,0 +1,18 @@
+/* Includes */
+#include "main.h"
+#include "ess_helper.h"
+
+int main(void)
+{
+	/* Initialize system */
+	HAL_Init();
+	/* Initialize peripherals on board */
+	ess_helper_init();
+
+	/* Set all the LEDs to on. */
+	*(uint32_t*) 0x40020C18 = 0xF000;
+
+    while(1)
+    {
+    }
+}
